c1b03c8a4b9cf3e2afe1a5650cd1b5b414ca399b
Santiago <santiago.pvazquez@gmail.com>
2024-03-11 15:55:41 -0600

    First Commit

---
+++ src/auth/routes/AuthRoutes.jsx
@@ -0,0 +1,15 @@
+import { Navigate, Routes, Route } from "react-router-dom"
+import { RegisterPage, LoginPage } from "../pages"
+
+
+
+export const AuthRoutes = () => {
+  return (
+    <Routes>
+        <Route path="login" element={ <LoginPage /> }/>
+        <Route path="register" element={ <RegisterPage /> }/>
+
+        <Route path="/*" element={<Navigate to="/auth/login" />} />
+    </Routes>
+  )
+}
