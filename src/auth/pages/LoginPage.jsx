d294cf2e06413884dd6c91ce415ed0a9caaf4013
Santiago <santiago.pvazquez@gmail.com>
2024-03-11 18:48:08 -0600

    Login and Register pages done

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,8 +1,47 @@
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Link as RouterLink } from "react-router-dom"
+import { AuthLayout } from "../layout/AuthLayout"
 
 export const LoginPage = () => {
   return (
     <>
-        <h1>LoginPage</h1>
+      <AuthLayout title='Login'>
+          <form>
+            <Grid container>
+
+              <Grid item xs={ 12 } sx={{marginTop: 2}}>
+                <TextField label='Email' type='email' placeholder='example@example.com' fullWidth />
+              </Grid>
+
+              <Grid item xs={ 12 } sx={{marginTop: 2}}>
+                <TextField label='Password' type='password' placeholder='**********' fullWidth />
+              </Grid>
+
+              <Grid container spacing={ 2 } sx={{ marginBottom: 2, marginTop: 1 }}>
+                <Grid item xs={ 12 }>
+                  <Button variant='contained' fullWidth>
+                    Login
+                  </Button>
+                </Grid>
+
+                <Grid item xs={ 12 }>
+                  <Button variant='contained' fullWidth>
+                    <Google/>
+                      <Typography sx={{marginLeft: 1}}>Google</Typography>
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <Grid container direction='row' justifyContent='end'>
+                <Link color='inherit' to='/auth/register' component={ RouterLink } >
+                  Create account
+                </Link>
+              </Grid>
+
+            </Grid>
+          </form>
+      </AuthLayout>
     
     </>
   )
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,45 @@
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Link as RouterLink } from "react-router-dom"
+import { AuthLayout } from "../layout/AuthLayout"
 
 export const RegisterPage = () => {
   return (
     <>
-        <h1>RegisterPage</h1>
+        <AuthLayout title='Register'>
+        <form>
+            <Grid container>
+
+              <Grid item xs={ 12 } sx={{marginTop: 2}}>
+                <TextField label='Name' type='text' placeholder='Your name' fullWidth />
+              </Grid>
+
+              <Grid item xs={ 12 } sx={{marginTop: 2}}>
+                <TextField label='Email' type='email' placeholder='example@example.com' fullWidth />
+              </Grid>
+
+              <Grid item xs={ 12 } sx={{marginTop: 2}}>
+                <TextField label='Password' type='password' placeholder='**********' fullWidth />
+              </Grid>
+
+              <Grid container spacing={ 2 } sx={{ marginBottom: 2, marginTop: 1 }}>
+                <Grid item xs={ 12 }>
+                  <Button variant='contained' fullWidth>
+                    Register
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <Grid container direction='row' justifyContent='end'>
+                <Typography sx={{marginRight: 1}}>Already have an account?</Typography>
+                <Link color='inherit' to='/auth/login' component={ RouterLink } >
+                  Login
+                </Link>
+              </Grid>
+
+            </Grid>
+          </form>
+
+        </AuthLayout>
     </>
   )
 }
